db51e9c5447042598ed19186da9fe4ebb27e5ee0
zachary schumpert <buphnezz@gmail.com>
2018-02-16 09:32:06 -0800

    filled in the constructor function prototype to get and compile to html

--- zachary-patricia/scripts/article.js
+++ zachary-patricia/scripts/article.js
@@ -13,6 +13,12 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let $template = $('#bloggerArticles-template').html();
+
+  let templateRender = Handlebars.compile($template);
+
+  return templateRender(this);
+
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
