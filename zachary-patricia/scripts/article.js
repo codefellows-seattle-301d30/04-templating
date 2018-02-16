6e90fa90a9a4c7d01b332adfd33518b1501d72d1
zachary schumpert <buphnezz@gmail.com>
2018-02-16 10:30:46 -0800

    updated template so it resembled the previous lab's template

--- zachary-patricia/scripts/article.js
+++ zachary-patricia/scripts/article.js
@@ -18,15 +18,19 @@ Article.prototype.toHtml = function() {
   let templateRender = Handlebars.compile($template);
 
 
-
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+
+  this.authorOfArticle = this.author;
+
+  
+
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
 
-  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  this.publishStatus = `published ${this.daysAgo} days ago`;
 
   // REVIEW: The ternary operator above accomplishes this same logic.
   // if(this.publishedOn) {
