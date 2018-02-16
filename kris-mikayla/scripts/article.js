1a287037d5c7a45d9ae025481d9cd659345549d3
MMehelich <cassiel8198@gmail.com>
2018-02-16 11:06:37 -0800

    completed article.js, got filters poulated. One works one doesn't.

--- kris-mikayla/scripts/article.js
+++ kris-mikayla/scripts/article.js
@@ -12,8 +12,9 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
+  // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let $template = $('#blog-template').html();
+  
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -22,6 +23,7 @@ Article.prototype.toHtml = function() {
 
 
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  let templateRender = Handlebars.compile($template);
 
   // REVIEW: The ternary operator above accomplishes this same logic.
   // if(this.publishedOn) {
@@ -30,8 +32,8 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars provides to return your filled-in html template for THIS article.
-
+  // DONE: Use the method that Handlebars provides to return your filled-in html template for THIS article.
+  return templateRender(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
