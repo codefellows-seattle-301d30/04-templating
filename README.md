c0429d8fe0422110e9c640731fbcb8c461f16273
Suzanne Richman <suzanne@favoredfortune.com>
2018-02-16 12:06:00 -0800

    Updated README. Updated converted many functions to arrow functions in the articleView.js. Troubleshot errors in our Handlebar template and found an error in our article.js Article constructor. Removed zombie code.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Kilovolt Blog - Lab 04
 
 **Author**: Haron and Suzanne Richman
-**Version**: 1.0.3 (increment the patch/fix version number up if you make more commits past your first submission)
+**Version**: 1.0.5 (increment the patch/fix version number up if you make more commits past your first submission)
 
 ## Overview
 
@@ -33,15 +33,16 @@ We used IcoMoon icon font for navigation icons. We included the jQuery and Handl
 ## Change Log
 
 02-16-2018 9:45 am - Commit README and added Handlebars library CDN.
+
 02-16-2018 10:19 am - Added Handlebars library and created handlebars template for HTML. Also did first pull request. 
 
+02-16-2018 11:14 am - Updated README. Updated the .toHtml method on the Article constructor function to use Handlebar library to create our template with less code.
 
-
-
+02-16-2018 11:14 am - Updated README. Updated converted many functions to arrow functions in the articleView.js. Troubleshot errors in our Handlebar template and found an error in our article.js Article constructor. Removed zombie code. 
 
 ## Credits and Collaborations
 
-* Thanks to our instructors and TAs. 
+* Thanks to our instructors and TAs and especially our classmates Austin Matteson and Josh West. 
 * We used the jQuery library.
 * We referenced the jQuery cheat sheet: https://oscarotero.com/jquery/
 * We used HandleBars library.
--- haron-sooz/scripts/article.js
+++ haron-sooz/scripts/article.js
@@ -3,64 +3,57 @@
 let articles = [];
 
 function Article (rawDataObj) {
-  this.author = rawDataObj.author;
-  this.authorUrl = rawDataObj.authorUrl;
-  this.title = rawDataObj.title;
-  this.category = rawDataObj.category;
-  this.body = rawDataObj.body;
-  this.publishedOn = rawDataObj.publishedOn;
+  for(let key in rawDataObj){
+    this[key] = rawDataObj[key];
+  }
 }
 
 Article.prototype.toHtml = function() {
   // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
-  for(let key in rawDataObject){
-    this[key] = rawDataObject[key];
+  //Get the template from the HTML
+  let $template = $('#article-template').html();
 
-    //Get the template from the HTML
-    let $template = $('#article-template').html();
+  //Use Handlebars to compile the HTML , returns a funciton
+  let templateRender = Handlebars.compile($template);
 
-    //Use Handlebars to compile the HTML , returns a funciton
-    let templateRender = Handlebars.compile($template);
-
-    // REVIEW: If your template will use properties that aren't on the object yet, add them.
-    // Since your template can't hold any JS logic, we need to execute the logic here.
-    // The result is added to the object as a new property, which can then be referenced by key in the template.
-    // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  // REVIEW: If your template will use properties that aren't on the object yet, add them.
+  // Since your template can't hold any JS logic, we need to execute the logic here.
+  // The result is added to the object as a new property, which can then be referenced by key in the template.
+  // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
 
-    this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-    // REVIEW: The ternary operator above accomplishes this same logic.
-    // if(this.publishedOn) {
-    //   this.publishStatus = `published ${this.daysAgo} days ago`;
-    // } else {
-    //   this.publishStatus = '(draft)';
-    // }
+  // REVIEW: The ternary operator above accomplishes this same logic.
+  // if(this.publishedOn) {
+  //   this.publishStatus = `published ${this.daysAgo} days ago`;
+  // } else {
+  //   this.publishStatus = '(draft)';
+  // }
 
-    // DONE: Use the method that Handlebars provides to return your filled-in html template for THIS article.
+  // DONE: Use the method that Handlebars provides to return your filled-in html template for THIS article.
 
-    //Return the complied HTML
-    return templateRender(this);
+  //Return the complied HTML
+  return templateRender(this);
 
-  }
-
-  // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-
-  // Because the sort method takes just two arguments, which are a,b in this case (hence the parens). Versus the use of the the "articleObject" and "article in the .forEach method which only needs one parameter.
-
-  //In the case of the this constructor function and it's resulting object instances, this .sort() method needs to compare two object instance's properties from the object array and 'a,b' is a shorthand to do so without having to call each instance of the array.
-
-  rawData.sort((a,b) => {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  rawData.forEach(articleObject => {
-    articles.push(new Article(articleObject));
-  });
-
-  articles.forEach(article => {
-    $('#articles').append(article.toHtml());
-  });
 }
+
+// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
+
+// Because the sort method takes just two arguments, which are a,b in this case (hence the parens). Versus the use of the the "articleObject" and "article in the .forEach method which only needs one parameter.
+
+//In the case of the this constructor function and it's resulting object instances, this .sort() method needs to compare two object instance's properties from the object array and 'a,b' is a shorthand to do so without having to call each instance of the array.
+
+rawData.sort((a,b) => {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(articleObject => {
+  articles.push(new Article(articleObject));
+});
+
+articles.forEach(article => {
+  $('#articles').append(article.toHtml());
+});
